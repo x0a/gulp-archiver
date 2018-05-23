9071f6ac936fff3fb0e00789ace67f375026609a
x0a <x0a.cake@gmail.com>
2018-05-23 04:49:51 -0500

    rudementary typing

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,12 @@ import { WriteStream } from "tty";
 import { Transform } from "stream";
 
 declare class Archiver{
-    constructor(type: string, opts?: object);
-    /*** Push files to archive */
-    add(path: string): Transform;
-    /*** Close archive */
-    close(fileOut: string): ReadableStream;
-    static create(fileOut: string, opts?: object): Transform;
+	constructor(type: string, opts?: object);
+	/*** Push files to archive */
+	add(path: string): Transform;
+	/*** Close archive */
+	close(fileOut: string): ReadableStream;
+	static create(fileOut: string, opts?: object): Transform;
 }
 
 export = Archiver;
